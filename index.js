84d8e7239d897d00ccafd8ad3fb63ee1679fbf62
sjrdevelopment <stuj.robinson@gmail.com>
2023-12-12 11:53:51 +0000

    refactoring to separate functions/utils and additional validation

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import { turnLeft, turnRight, moveForwards } from './utils/movements.js'
+import * as readline from 'node:readline'
+import { stdin as input, stdout as output } from 'node:process'
+
 const gridSize = {
     x: 0,
     y: 0,
@@ -12,127 +16,66 @@ const offGridPoints = []
 const commandSizeLimit = 100
 const coordinateSizeLimit = 50
 
-const readline = require('node:readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
+const rl = readline.createInterface({
+    input,
+    output,
 })
 
-const moveForwards = (coordinates, heading) => {
-    let newCoordinates = { ...coordinates }
-    let lost = false
-    switch (heading) {
-        case 'N':
-            newCoordinates.y += 1
-            break
-        case 'E':
-            newCoordinates.x += 1
-            break
-        case 'S':
-            newCoordinates.y -= 1
-            break
-        case 'W':
-            newCoordinates.x -= 1
-            break
-    }
-
-    if (
-        offGridPoints.find(
-            (coord) =>
-                coord.x === newCoordinates.x && coord.y === newCoordinates.y
-        )
-    ) {
-        // ignore this, we;re about to lose the robot!
-        newCoordinates = { ...coordinates }
-    }
-    if (
-        newCoordinates.y < 0 ||
-        newCoordinates.x < 0 ||
-        newCoordinates.y > gridSize.y ||
-        newCoordinates.x > gridSize.x
-    ) {
-        console.log('LOST')
-        offGridPoints.push(newCoordinates) // add the coordinate that caused the robot to go off grid
-        newCoordinates = { ...coordinates }
-        lost = true
-    }
-
-    return [newCoordinates, heading, lost]
-}
-
-const turnLeft = (heading) => {
-    const reversedCompassPoints = compassPoints.slice(0).reverse()
-    const compassIndex = reversedCompassPoints.indexOf(heading)
-    const newHeading =
-        reversedCompassPoints[(compassIndex + 1) % reversedCompassPoints.length]
-
-    return newHeading
-}
-
-const turnRight = (heading) => {
-    const compassIndex = compassPoints.indexOf(heading)
-    const newHeading = compassPoints[(compassIndex + 1) % compassPoints.length]
-
-    return newHeading
-}
-
 const moveFromStartingPosition = (gridSize) => {
-    readline.question(
+    rl.question(
         `Enter start position for next robot (within ${gridSize.x},${gridSize.y}): `,
         (startPosition) => {
-            console.log(`Setting start position ${startPosition}`)
-
             const startPosArr = startPosition.split(' ') // x y heading
             let currentCoordinates = {
                 x: parseInt(startPosArr[0]),
                 y: parseInt(startPosArr[1]),
             }
-            let currentHeading = startPosArr[2]
+            let currentHeading = startPosArr[2].toUpperCase()
 
-            readline.question('Enter command string: ', (command) => {
-                console.log(`Moving ${command}`)
-
+            rl.question('Enter command string: ', (command) => {
                 if (command.length > commandSizeLimit) {
                     throw new Error(
                         `Command length exceeds limit of ${commandSizeLimit}`
                     )
-                    // TODO: put parameter constants in consts
                 }
 
                 let isLost = false
 
-                command.split('').forEach((command) => {
-                    console.log(isLost)
-                    if (!allowedCommands.includes(command)) {
-                        console.error(
-                            'Command not allowed, skipping to next command'
-                        )
-                    } else if (!isLost) {
-                        switch (command) {
-                            case 'F':
-                                console.log('moving forwards')
-                                ;[currentCoordinates, currentHeading, isLost] =
-                                    moveForwards(
-                                        currentCoordinates,
-                                        currentHeading
+                command
+                    .toUpperCase()
+                    .split('')
+                    .forEach((command) => {
+                        if (!allowedCommands.includes(command)) {
+                            console.error(
+                                'Command not allowed, skipping to next command'
+                            )
+                        } else if (!isLost) {
+                            switch (command) {
+                                case 'F':
+                                    ;[currentCoordinates, isLost] =
+                                        moveForwards(
+                                            currentCoordinates,
+                                            currentHeading,
+                                            offGridPoints,
+                                            gridSize
+                                        )
+                                    break
+                                case 'L':
+                                    currentHeading = turnLeft(
+                                        currentHeading,
+                                        compassPoints
                                     )
-
-                                console.log(currentCoordinates)
-                                break
-                            case 'L':
-                                console.log('turning left')
-                                currentHeading = turnLeft(currentHeading)
-                                console.log('new heading ' + currentHeading)
-                                break
-                            case 'R':
-                                console.log('turning right')
-                                currentHeading = turnRight(currentHeading)
-                                console.log('new heading ' + currentHeading)
-                                break
+                                    break
+                                case 'R':
+                                    currentHeading = turnRight(
+                                        currentHeading,
+                                        compassPoints
+                                    )
+                                    break
+                            }
                         }
-                    }
-                })
+                    })
 
-                console.log('final position: ')
                 console.log(
                     `${currentCoordinates.x} ${
                         currentCoordinates.y
@@ -144,17 +87,14 @@ const moveFromStartingPosition = (gridSize) => {
     )
 }
 
-readline.question(
+rl.question(
     'Enter grid max coordinates separated by a space: ',
     (gridMaxCoordinates) => {
-        console.log(`Setting grid size ${gridMaxCoordinates}`)
-
         const gridSizeCoordinates = gridMaxCoordinates
             .split(' ')
             .map((coordinate) => {
                 const returnVal = parseInt(coordinate)
-                console.log('return is')
-                console.log(returnVal)
+
                 if (isNaN(returnVal)) {
                     throw new Error(`Invalid grid coordinate ${coordinate}`)
                 } else {
---
+++ utils/movements.js
@@ -0,0 +1,56 @@
+const moveForwards = (coordinates, heading, offGridPoints, gridSize) => {
+    let newCoordinates = { ...coordinates }
+    let lost = false
+    switch (heading) {
+        case 'N':
+            newCoordinates.y += 1
+            break
+        case 'E':
+            newCoordinates.x += 1
+            break
+        case 'S':
+            newCoordinates.y -= 1
+            break
+        case 'W':
+            newCoordinates.x -= 1
+            break
+    }
+
+    if (
+        offGridPoints.find(
+            (coord) =>
+                coord.x === newCoordinates.x && coord.y === newCoordinates.y
+        )
+    ) {
+        // ignore this, we're about to lose the robot!
+        newCoordinates = { ...coordinates }
+    } else if (
+        newCoordinates.y < 0 ||
+        newCoordinates.x < 0 ||
+        newCoordinates.y > gridSize.y ||
+        newCoordinates.x > gridSize.x
+    ) {
+        offGridPoints.push(newCoordinates)
+        newCoordinates = { ...coordinates }
+        lost = true
+    }
+
+    return [newCoordinates, lost]
+}
+
+const turnLeft = (heading, compassPoints) => {
+    const reversedCompassPoints = compassPoints.slice(0).reverse()
+    const compassIndex = reversedCompassPoints.indexOf(heading)
+    const newHeading =
+        reversedCompassPoints[(compassIndex + 1) % reversedCompassPoints.length]
+
+    return newHeading
+}
+
+const turnRight = (heading, compassPoints) => {
+    const compassIndex = compassPoints.indexOf(heading)
+    const newHeading = compassPoints[(compassIndex + 1) % compassPoints.length]
+
+    return newHeading
+}
+export { turnRight, turnLeft, moveForwards }
